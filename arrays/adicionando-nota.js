23a715da47b7ba336327cd1b54c50d527ac2d7de
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-07-25 17:03:17 -0300

    25/07/2023

---
+++ arrays/adicionando-nota.js
@@ -0,0 +1,8 @@
+const notas = [10, 6, 8];
+
+notas.push(7) // usado para adiconar um valor no final do array no caso esse seria [4]
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(`A media é ${media}`);
+
