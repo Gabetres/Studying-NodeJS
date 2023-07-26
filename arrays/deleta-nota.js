86cbf380b33bc23905b04610a06273437e80d17c
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-07-25 23:58:28 -0300

    26/07/2023

--- arrays/deleta-nota.js
+++ arrays/deleta-nota.js
@@ -1,6 +1,8 @@
 const notas = [10, 6, 8, 5.5, 10];
 
-notas.pop();
+notas.pop(); // remove o ultimo elemento do array
+
+console.log(notas)
 
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
---
+++ arrays/grupos.js
@@ -0,0 +1,29 @@
+const alunos = [
+    'João',
+    'Juliana',
+    'Ana',
+    'Hakaink',
+    'Lara',
+    'Junin_dev',
+    'Guilherme',
+    'Aline',
+    'Fabiana',
+    'Andre',
+    'Carlos',
+    'Otaldodoge',
+    'Bia',
+    'Vivian',
+    'Isabela',
+    'Vinícius',
+    'Renan',
+    'Renata',
+    'Daisy',
+    'Camilo'];
+
+const sala1 = alunos.slice(0, (alunos.length/2));
+
+const sala2 = alunos.slice(alunos.length/2);
+
+
+console.log(sala1);
+console.log(sala2);
